165e163641135140508bae71ee7bffd9848881a7
Suraj Thapa <stsurajst@gmail.com>
2019-02-15 16:33:38 +0545

    burger build finished

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,7 +3,13 @@ import classses from "./BuildControl.css";
 const buildControl = props => (
   <div className={classses.BuildControl}>
     <div className={classses.Label}>{props.label}</div>
-    <button className={classses.Less}>Less</button>
+    <button
+      className={classses.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
     <button className={classses.More} onClick={props.added}>
       More
     </button>
